b13592e8fb35937c9d4eca49ee8d3b385e15f616
Yurii Makarov <makaroff.seven@gmail.com>
2024-03-18 18:49:51 +0100

    [Fix] Match properties were protected

--- src/entities/match/index.ts
+++ src/entities/match/index.ts
@@ -4,11 +4,11 @@ import IMatch, { MatchStatus, Team } from "../../interfaces/match.interface";
 import Announcer from "../announcer";
 
 export default class Match implements IMatch {
-  homeTeam: Team;
-  awayTeam: Team;
-  score: number;
-  time: number;
-  status: MatchStatus;
+  private _homeTeam: Team;
+  private _awayTeam: Team;
+  private _score: number;
+  private _time: number;
+  private _status: MatchStatus;
 
   constructor({ homeTeamName, awayTeamName }: Record<string, string>) {
     if (homeTeamName === awayTeamName) {
@@ -17,49 +17,65 @@ export default class Match implements IMatch {
 
     const score = 0;
 
-    this.homeTeam = { name: homeTeamName, score };
-    this.awayTeam = { name: awayTeamName, score };
+    this._homeTeam = { name: homeTeamName, score };
+    this._awayTeam = { name: awayTeamName, score };
 
     return;
   }
 
+  get homeTeam() {
+    return this._homeTeam;
+  }
+  get awayTeam() {
+    return this._awayTeam;
+  }
+  get score() {
+    return this._score;
+  }
+  get time() {
+    return this._time;
+  }
+  get status() {
+    return this._status;
+  }
+
   startMatch(): string {
-    this.time = Date.now();
-    this.status = MATCH_STATUSES.STARTED;
+    this._time = Date.now();
+    this._status = MATCH_STATUSES.STARTED;
 
     return Announcer.matchBegin({
-      homeTeamName: this.homeTeam.name,
-      awayTeamName: this.awayTeam.name,
+      homeTeamName: this._homeTeam.name,
+      awayTeamName: this._awayTeam.name,
     });
   }
 
   updateMatch(homeScore: number, awayScore: number): string {
     //It allows to take 1 point, if the decission was worng
     if (
-      homeScore < this.homeTeam.score - 1 ||
-      awayScore < this.awayTeam.score - 1
+      homeScore < this._homeTeam.score - 1 ||
+      awayScore < this._awayTeam.score - 1
     ) {
       throw new Error(ERRORS_NAMES.INCORRECT_VALUE);
     }
 
-    this.homeTeam.score = homeScore;
-    this.awayTeam.score = awayScore;
-    this.score = homeScore + awayScore;
+    this._homeTeam.score = homeScore;
+    this._awayTeam.score = awayScore;
+    this._score = homeScore + awayScore;
 
     return Announcer.matchUpdated({
-      homeTeam: this.homeTeam,
-      awayTeam: this.awayTeam,
+      homeTeam: this._homeTeam,
+      awayTeam: this._awayTeam,
     });
   }
 
   finishMatch(): string {
     let result: string;
-    this.status = MATCH_STATUSES.IN_PROGRESS;
+    this._status = MATCH_STATUSES.IN_PROGRESS;
 
-    if (this.homeTeam.score === this.awayTeam.score) {
+    if (this._homeTeam.score === this._awayTeam.score) {
       result = "Draw!";
     } else {
-      result = `${this.homeTeam.name} ${this.homeTeam.score} - ${this.awayTeam.name} ${this.awayTeam.score}.`;
+      result = `${this._homeTeam.name} ${this._homeTeam.score} - ${this._awayTeam.name} ${this._awayTeam.score}.`;
     }
 
     return Announcer.matchFinished(result);
